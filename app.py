7086ef63feb1bd884d49261da0bee621f316bce8
Rajithra-R <145992978+Rajithra-R@users.noreply.github.com>
2025-07-19 23:56:29 +0530

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from fpdf import FPDF
 from docx import Document
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import WebBaseLoader
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
